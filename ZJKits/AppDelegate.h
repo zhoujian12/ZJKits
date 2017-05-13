8633164d30539b9cedd96cf0657b53a45c1eccfd
jianzhouUp <jianz3>
2017-05-13 16:45:14 +0800

    new branch for JZKits

---
+++ ZJKits/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJKits
+//
+//  Created by jianz3 on 2017/5/13.
+//  Copyright © 2017年 jianz3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJKits/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJKits
+//
+//  Created by jianz3 on 2017/5/13.
+//  Copyright © 2017年 jianz3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
