2cfa9d55f55fb3e138a88bf00b0ecf14780e4fc0
jianzhouUp <jianz3>
2017-07-25 18:10:57 +0800

    增加视图坐标属性扩展类

---
+++ UIView+position.h
@@ -0,0 +1,13 @@
+//
+//  UIView+position.h
+//  ZJKits
+//
+//  Created by jianz3 on 2017/7/25.
+//  Copyright © 2017年 jianz3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (position)
+
+@end
---
+++ UIView+position.m
@@ -0,0 +1,13 @@
+//
+//  UIView+position.m
+//  ZJKits
+//
+//  Created by jianz3 on 2017/7/25.
+//  Copyright © 2017年 jianz3. All rights reserved.
+//
+
+#import "UIView+position.h"
+
+@implementation UIView (position)
+
+@end
